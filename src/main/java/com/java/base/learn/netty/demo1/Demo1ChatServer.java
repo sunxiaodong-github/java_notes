644b4181affc36f831416af2207d2cb88a155f27
ShangzhongSun <13764687527@163.com>
2020-04-01 23:59:25 +0800

    netty多客户端连接demo 服务端编写完成

---
+++ src/main/java/com/java/base/learn/netty/demo1/Demo1ChatServer.java
@@ -0,0 +1,35 @@
+package com.java.base.learn.netty.demo1;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * <p>
+ *     简易聊天程序
+ * </p>
+ *
+ * @author xiaodongsun
+ * @date 2020/4/1 11:26 PM
+ */
+public class Demo1ChatServer {
+
+    public static void main(String[] args) throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new Demo1ChatServerInitializer());
+            ChannelFuture channelFuture = serverBootstrap.bind(8888).sync();
+            channelFuture.channel().closeFuture().sync();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
