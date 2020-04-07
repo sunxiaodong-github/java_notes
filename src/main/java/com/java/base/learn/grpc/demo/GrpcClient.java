0908265824e527796ffa1421d142fc6e66c2b3c4
sunxiaodong <sunxiaodong_mail@163.com>
2020-04-07 17:24:47 +0800

    gRPC通信示例以及JVM回调钩子

---
+++ src/main/java/com/java/base/learn/grpc/demo/GrpcClient.java
@@ -0,0 +1,23 @@
+package com.java.base.learn.grpc.demo;
+
+import com.java.base.learn.grpc.MyRequest;
+import com.java.base.learn.grpc.MyResponse;
+import com.java.base.learn.grpc.StudentServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+/**
+ * <p></P>
+ *
+ * @author sunxiaodong
+ * @date 20-4-7 上午4:50
+ **/
+public class GrpcClient {
+
+    public static void main(String[] args) {
+        ManagedChannel managedChannel = ManagedChannelBuilder.forAddress("localhost", 8888).usePlaintext().build();
+        StudentServiceGrpc.StudentServiceBlockingStub blockingStub = StudentServiceGrpc.newBlockingStub(managedChannel);
+        MyResponse myResponse = blockingStub.getRealNameByUsername(MyRequest.newBuilder().setUsername("偏执狂").build());
+        System.out.println(myResponse.getRealname());
+    }
+}
---
+++ src/main/java/com/java/base/learn/grpc/demo/GrpcServer.java
@@ -0,0 +1,48 @@
+package com.java.base.learn.grpc.demo;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+/**
+ * <p></P>
+ *
+ * @author sunxiaodong
+ * @date 20-4-7 上午4:50
+ **/
+public class GrpcServer {
+
+    private Server server;
+
+    private void start() throws IOException {
+        this.server = ServerBuilder.forPort(8888).addService(new StudentServiceImpl()).build().start();
+        System.out.println("server started！");
+
+        // 回调钩子函数
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("关闭jvm");
+            GrpcServer.this.stop();
+        }));
+
+        System.out.println("执行到这里");
+    }
+
+    private void stop(){
+        if (null != this.server){
+            this.server.shutdown();
+        }
+    }
+
+    private void awaitTermination() throws InterruptedException {
+        if (null != this.server){
+            this.server.awaitTermination();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        GrpcServer server = new GrpcServer();
+        server.start();
+        server.awaitTermination();
+    }
+}
