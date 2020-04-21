930fa7191dea81462a578246ce6417fec0f24322
sunxiaodong <sunxiaodong_mail@163.com>
2020-04-22 00:44:25 +0800

    JVM笔记 类初始化加载

--- src/main/java/com/java/base/learn/jvm/Demo5.java
+++ src/main/java/com/java/base/learn/jvm/Demo5.java
@@ -1,8 +1,11 @@
 package com.java.base.learn.jvm;
 
+import java.util.Random;
+
 /**
  * <p>
  *     当一个接口在初始化时，并不是要求其父接口都完成了初始化
+ *     只有在真正使用到父接口的时候（如引用接口中所定义的常量时），才会初始化。
  * </p>
  *
  * @author sunxiaodong
@@ -15,8 +18,8 @@ public class Demo5 {
     }
 }
 interface Parent5 {
-    public static int a = 5;
+    public static final int a = new Random().nextInt(3);
 }
 interface Child5 extends Parent5 {
-    public static int b = 6;
+    public static final int b = new Random().nextInt(4);
 }
---
+++ src/main/java/com/java/base/learn/jvm/Demo6.java
@@ -0,0 +1,32 @@
+package com.java.base.learn.jvm;
+
+/**
+ * <p></p>
+ *
+ * @author sunxiaodong
+ * @date 2020/4/21 9:58 PM
+ */
+public class Demo6 {
+
+    public static void main(String[] args) {
+        Singleton singleton = Singleton.getInstance();
+        System.out.println("counter1: " + Singleton.counter1);
+        System.out.println("counter2: " + Singleton.counter2);
+    }
+}
+
+class Singleton{
+    public static int counter1;
+    //public static int counter2 = 0;
+
+    private static Singleton singleton = new Singleton();
+
+    private Singleton(){
+        counter1 ++;
+        counter2 ++; //准备阶段的重要意义
+    }
+    public static int counter2 = 0;
+    public static Singleton getInstance(){
+        return singleton;
+    }
+}
