05c3ab9c5af0e0d75dc924707887940ba945ca05
sunxiaodong <sunxiaodong_mail@163.com>
2020-04-29 00:15:56 +0800

    类加载器的命名空间

--- src/main/java/com/java/base/learn/jvm/Demo16.java
+++ src/main/java/com/java/base/learn/jvm/Demo16.java
@@ -68,11 +68,27 @@ public class Demo16 extends ClassLoader {
     }
 
     public static void main(String[] args) throws IllegalAccessException, InstantiationException, ClassNotFoundException {
-        Demo16 loader = new Demo16("loader");
-        loader.setPath("/home/sunxiaodong/Desktop/");
-        Class<?> clazz = loader.loadClass("com.java.base.learn.jvm.Demo1");
-        Object instance = clazz.newInstance();
-        System.out.println(instance);
+        Demo16 loader1 = new Demo16("loader1");
+        loader1.setPath("/Users/sunxiaodong/Desktop/");
+        Class<?> clazz1 = loader1.loadClass("com.java.base.learn.jvm.Demo1");
+        System.out.println("class1: " + clazz1.hashCode());
+        Object instance1 = clazz1.newInstance();
+        System.out.println(instance1);
+
+        System.out.println();
+        /**
+         * 命名空间
+         * 每个类加载器都有自己的命名空间，命名空间由该加载器及所有父加载器所加载的类组成
+         * 在同一个命名空间中，不会出现类的完整名字（包括类的包名）相同的两个类
+         * 在不同的命名空间中，有可能会出现类的完整名字（包括类的包名）相同的两个类
+         */
+        //Demo16 loader2 = new Demo16("loader2");
+        Demo16 loader2 = new Demo16(loader1, "loader2");
+        loader2.setPath("/Users/sunxiaodong/Desktop/");
+        Class<?> clazz2 = loader2.loadClass("com.java.base.learn.jvm.Demo1");
+        System.out.println("class2: " + clazz2.hashCode());
+        Object instance2 = clazz2.newInstance();
+        System.out.println(instance2);
     }
 
 }
