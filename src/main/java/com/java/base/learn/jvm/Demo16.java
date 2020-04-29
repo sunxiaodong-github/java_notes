4b1b14d19f86f9608105b008ba303bd85b181e16
sunxiaodong <sunxiaodong_mail@163.com>
2020-04-29 16:32:13 +0800

    自定义类加载器的卸载

--- src/main/java/com/java/base/learn/jvm/Demo16.java
+++ src/main/java/com/java/base/learn/jvm/Demo16.java
@@ -67,15 +67,29 @@ public class Demo16 extends ClassLoader {
         return data;
     }
 
-    public static void main(String[] args) throws IllegalAccessException, InstantiationException, ClassNotFoundException {
+    public static void main(String[] args) throws IllegalAccessException, InstantiationException, ClassNotFoundException, InterruptedException {
         Demo16 loader1 = new Demo16("loader1");
-        loader1.setPath("/Users/sunxiaodong/Desktop/");
+        loader1.setPath("/home/sunxiaodong/Desktop/");
         Class<?> clazz1 = loader1.loadClass("com.java.base.learn.jvm.Demo1");
         System.out.println("class1: " + clazz1.hashCode());
         Object instance1 = clazz1.newInstance();
         System.out.println(instance1);
 
         System.out.println();
+
+        loader1 = null;
+        clazz1 = null;
+        instance1 = null;
+        System.gc();
+
+        Thread.sleep(200000);
+
+        loader1 = new Demo16("loader1");
+        loader1.setPath("/home/sunxiaodong/Desktop/");
+        clazz1 = loader1.loadClass("com.java.base.learn.jvm.Demo1");
+        System.out.println("class1: " + clazz1.hashCode());
+        instance1 = clazz1.newInstance();
+        System.out.println(instance1);
         /**
          * 命名空间
          * 每个类加载器都有自己的命名空间，命名空间由该加载器及所有父加载器所加载的类组成
@@ -83,12 +97,21 @@ public class Demo16 extends ClassLoader {
          * 在不同的命名空间中，有可能会出现类的完整名字（包括类的包名）相同的两个类
          */
         //Demo16 loader2 = new Demo16("loader2");
-        Demo16 loader2 = new Demo16(loader1, "loader2");
-        loader2.setPath("/Users/sunxiaodong/Desktop/");
+        /*Demo16 loader2 = new Demo16(loader1, "loader2");
+        loader2.setPath("/home/sunxiaodong/Desktop/");
         Class<?> clazz2 = loader2.loadClass("com.java.base.learn.jvm.Demo1");
         System.out.println("class2: " + clazz2.hashCode());
         Object instance2 = clazz2.newInstance();
         System.out.println(instance2);
+
+        System.out.println();
+        Demo16 loader3 = new Demo16(loader2,"loader3");
+        loader3.setPath("/home/sunxiaodong/Desktop/");
+        Class<?> clazz3 = loader3.loadClass("com.java.base.learn.jvm.Demo1");
+        System.out.println("class2: " + clazz3.hashCode());
+        Object instance3 = clazz3.newInstance();
+        System.out.println(instance3);*/
+
     }
 
 }
